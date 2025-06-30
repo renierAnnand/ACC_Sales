b1a666f45023075c175f41c09b6bf26ca5b36c31
renierAnnand <152157316+renierAnnand@users.noreply.github.com>
2025-06-30 07:59:57 +0300

    sales_forecasting.py

---
+++ sales_forecasting.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def run():
+    st.header("Sales Forecasting")
+    st.write("This module will use models like Prophet and XGBoost to forecast future sales.")
+    st.warning("Forecasting models will be implemented soon.")
